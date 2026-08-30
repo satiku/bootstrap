2dbe54cdcc8140656cd800a731f5957ddc4158b9
satiku@gmail.com <satiku@gmail.com>
2026-08-30 01:48:46 +0000

    add banners and window manager packages

--- archusb/setup.sh
+++ archusb/setup.sh
@@ -3,19 +3,38 @@
 
 
 packages=(
-	neovim
-	qutebrowser
-	mpv
-	btop
-	yazi
+	xorg-server
+	xorg-xinit
+	i3-wm
+	i3status
+	i3blocks
+	i3lock
+	dmenu
+	alacritty     # terminal emulator
+	neovim        # text editor
+	qutebrowser   # web browser
+	mpv           # media player
+	btop          # resource monitor
+	yazi          # file manager
 	ueberzugpp    # yazi 
 	poppler       # yazi 
 	p7zip         # yazi
 )
 
 
+echo ""
+echo "#############################"
+echo "UPDATE REPOS"
+echo "#############################"
+echo ""
 
 sudo pacman -Sy
+
+echo ""
+echo "#############################"
+echo "INSTALL PACKAGES"
+echo "#############################"
+echo ""
 
 for pkg in "${packages[@]}"; do 
 	if ! sudo pacman -S --noconfirm  --needed "$pkg"; then 
