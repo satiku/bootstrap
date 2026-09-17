d752b936e59f9042cc1eb99f7bebcd60c190df32
satiku <43967637+satiku@users.noreply.github.com>
2026-09-17 16:06:06 +0800

    Fix broken yay install in setup.sh (#3)
    * Fix broken yay install: drop backticks, use temp dir, noconfirm
    
    * Make yay install resilient: yay-bin + GitHub fallback
    
    * Simplify yay install to short yay-bin clone

--- archusb/setup.sh
+++ archusb/setup.sh
@@ -184,19 +184,20 @@ fi
 
 
 
-# Install AUR YAY 
+# Install AUR helper (yay-bin avoids Go compile / connection refused)
 
 header "Install AUR"
 
-if ! yay &> /dev/null ; then 
-
-	if ! ` sudo pacman -S  --noconfirm --needed base-devel git go &> /dev/null && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si ` ; then 
-		fail "Install yay"
-	else
-	
+if command -v yay &>/dev/null; then
+	blue "yay"
+else
+	yay_build="$(mktemp -d)"
+	if sudo pacman -S --noconfirm --needed base-devel git &>/dev/null \
+		&& git clone --depth 1 https://aur.archlinux.org/yay-bin.git "$yay_build" \
+		&& (cd "$yay_build" && makepkg -si --noconfirm); then
 		pass "Install yay"
+	else
+		fail "Install yay"
 	fi
-else 
-	blue "Install yay"
-  
+	rm -rf "$yay_build"
 fi
